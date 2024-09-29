800e19ac46f630ec22ee61b0060a592c05e2d572
Nathan Jason <jasonlowen30@gmail.com>
2024-09-29 21:24:09 +0700

    hilangin border component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div className="border-b border-e-neutral-900 pb-20">
+    <div className="pb-20">
       <motion.h2
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
